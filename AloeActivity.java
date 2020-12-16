8e889fa76edbabed993e9fb2adfe986d5ef37a13
DanianiWong-web <75284535+DanianiWong-web@users.noreply.github.com>
2020-12-16 12:06:09 -0500

    Add files via upload

---
+++ AloeActivity.java
@@ -0,0 +1,14 @@
+package com.example.plantparenthud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AloeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aloe);
+    }
+}
