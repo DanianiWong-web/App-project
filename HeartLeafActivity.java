9031dde3b0c29a4922a41f5c9e6a2ba58768e854
DanianiWong-web <75284535+DanianiWong-web@users.noreply.github.com>
2020-12-16 13:26:21 -0500

    Add files via upload

---
+++ HeartLeafActivity.java
@@ -0,0 +1,14 @@
+package com.example.plantparenthud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeartLeafActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heart_leaf);
+    }
+}
